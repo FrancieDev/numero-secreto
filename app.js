c2bf9138d3a33b42fba4e4c422c9a0b62303547d
FrancieDev <francielima.pro@gmail.com>
2024-02-18 10:07:15 -0300

    Limite alterado para 50

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 100';
+paragrafo.innerHTML = 'Escolha um número entre 1 e 50';
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -14,13 +14,13 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
 exibirMensagemInicial();
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
